c29b7098f77600d2cebcf4e236f15010f1e59e04
Rajusavra <savraraju49@gmail.com>
2025-01-10 15:17:45 +0530

    10-1-25     All javascript data

---
+++ Detailed Js/01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "Sparky"
+const repoCount = 50
+
+// console.log(name + repoCount + "theCoder");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('call-Of-Duty') 
+
+// console.log(gameName[1]);
+// console.log(gameName.slice(4,gameName.length));
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(8));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,6)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-2, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   Sparky    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://Sparky.com/Sparky%20choudhary"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('.co'));
+
+console.log(gameName.split('-'));
---
+++ Detailed Js/01_basics/06_nums_and_math.js
@@ -0,0 +1,51 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// Add mathematical decimals-------------------------------------------
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+
+// Give a value acording to rule of maths------------------------------
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+//  Give a local string according to cuntries countation ex en_IN for indian value----------------------
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+console.log(Math);
+
+// Return a Absolute(+) value
+
+// console.log(Math.abs(-4));
+
+// round of with maths rule to normal number from decimal------------------
+
+// console.log(Math.round(4.6));
+
+// console.log(Math.ceil(4.2));
+
+//  Round up in Lovest value-------------------------------
+
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+//  give an value between 0 to 1-----------------------
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
